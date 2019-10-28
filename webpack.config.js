e6233253b6b5caa75c41b9724e1a1c061c3d2371
LeonardoCamargo31 <leonardo_camargo31@hotmail.com>
2019-10-28 11:26:46 -0300

    add webpack

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const path = require('path');
+
+const config = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    // Universal Module Definition, trabalha com amd, CommonJS e para navegadores com prototypes
+    // e guardamos na dist esses arquivos em umd, que podem ser utilizados
+    // tanto no node quanto no navegador
+    libraryTarget: 'umd',
+    library: 'spotifyWrapper',
+  },
+  module: {
+    rules: [
+      {
+        // chamando o loadar do babel, já que nosso código esta em es6 fazer o traspiler
+        test: /\.js$/,
+        use: 'babel-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+};
+
+module.exports = config;
